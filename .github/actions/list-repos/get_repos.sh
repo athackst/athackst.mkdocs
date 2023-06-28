677d5e82a1ab0a3c31cc237d4835c41d4119c65a
Allison Thackston <allison@allisonthackston.com>
2023-06-28 05:54:54 +0000

    Hanlde pagination in the list-repos action.

--- .github/actions/list-repos/get_repos.sh
+++ .github/actions/list-repos/get_repos.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-gh api /users/$USER/repos | jq -c --arg SELECTED "$SELECTED" "[.[] 
-    | select(.private==$PRIVATE and .fork==$FORK and .archived==$ARCHIVED)
-    | .full_name 
-    ]"
+# gh api "/users/$USER/repos?page=1&per_page=100"  | jq -c --arg SELECTED "$SELECTED" "[.[] 
+#     | select(.private==$PRIVATE and .fork==$FORK and .archived==$ARCHIVED)
+#     | .full_name 
+#     ]"
+
+page=1
+repositories=()
+
+while : ; do
+  response=$(gh api "/users/$USER/repos?page=$page&per_page=100")
+  page_repositories=$(echo "$response" | jq -c --arg SELECTED "$SELECTED" "[.[] | select(.private==$PRIVATE and .fork==$FORK and .archived==$ARCHIVED) | .full_name ]")
+  repositories+=($page_repositories)
+
+  if [[ $(echo "$response" | jq '. | length') -lt 100 ]]; then
+    break
+  fi
+
+  page=$((page + 1))
+done
+
+echo "${repositories[@]}"
